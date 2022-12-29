c0c9f336577241aa76410b02c5c3c6ba39af800d
Néstor Pérez <v-nolvera@expediagroup.com>
2022-12-29 09:13:30 -0600

    Added log to okhttpclient

--- src/main/kotlin/dev/nestorperez/registrybackend/RegistryBackendApplication.kt
+++ src/main/kotlin/dev/nestorperez/registrybackend/RegistryBackendApplication.kt
@@ -5,21 +5,34 @@ import dev.nestorperez.registrybackend.registry.RegistryApi
 import dev.nestorperez.registrybackend.registry.interceptors.ErrorHandlingInterceptor
 import dev.nestorperez.registrybackend.util.SkipCoverage
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.core.env.Environment
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 @SpringBootApplication
 class RegistryBackendApplication {
 
+    /**
+     * If current active profile is not production, then set the log level to body otherwise set it to basic
+     */
+    fun loggingInterceptor(environment: Environment) = HttpLoggingInterceptor().apply {
+        level = if (environment.activeProfiles.contains("prod")) {
+            HttpLoggingInterceptor.Level.BASIC
+        } else {
+            HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
     @Bean
-    fun okHttpClient() = OkHttpClient
-        .Builder()
+    fun okHttpClient(environment: Environment) = OkHttpClient.Builder()
         .addInterceptor(ContextInterceptor())
         .addInterceptor(ErrorHandlingInterceptor())
+        .addInterceptor(loggingInterceptor(environment))
         .build()
 
     @Bean
